fecfea4199a7813c2152b8cbad3dfc7de61c4ad7
tk <>
2021-03-21 13:03:09 +0000

    修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,24 @@
 class TasksController < ApplicationController
   def index
-   @tasks = Task.all
+  @tasks = Task.all
   end
 
-  def show
-   @task = Task.find(params[:id])
+ def show
+  @task = Task.find(params[:id])
   end
 
-  def new
+ def new
   @task = Task.new
   end
 
   def create
-@task = Task.new(task_params)
+  @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
+      flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash.now[:danger] = 'タスクが投稿されませんでした'
       render :new
     end
   end
@@ -31,25 +31,25 @@ class TasksController < ApplicationController
    @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
     end
   end
 
   def destroy
-      @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'task は正常に削除されました'
+    flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
-end
  private
 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
   end
+end
