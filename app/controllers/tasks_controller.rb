c7e4a60e5103bdf751f5c80ed6a088ca631429f9
tk <>
2021-04-08 01:25:37 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,8 @@ class TasksController < ApplicationController
   def set_task
     @task = Task.find(params[:id])
   end
+ 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
